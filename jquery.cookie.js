46ac7d1268602c1e0b5ffb0f2c4bb59608b4a0e0
Jean-Sebastien Ney <jeansebastien.ney@gmail.com>
2012-02-16 14:11:17 +0100

    help minification

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -7,14 +7,14 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.opensource.org/licenses/GPL-2.0
  */
-(function($) {
+(function($, nil, emptyString, undefined) {
   $.cookie = function(key, value, options) {
 
     // key and at least value given, set cookie...
-    if (arguments.length > 1 && (!/Object/.test(Object.prototype.toString.call(value)) || value === null || value === undefined)) {
+    if (arguments.length && (!/Object/.test(Object.prototype.toString.call(value)) || value === nil || value === undefined)) {
       options = $.extend({}, options);
 
-      if (value === null || value === undefined) {
+      if (value === nil || value === undefined) {
         options.expires = -1;
       }
 
@@ -25,13 +25,15 @@
 
       value = String(value);
 
-      return (document.cookie = [
-              encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
-              options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-              options.path    ? '; path=' + options.path : '',
-              options.domain  ? '; domain=' + options.domain : '',
-              options.secure  ? '; secure' : ''
-      ].join(''));
+      return (
+        document.cookie = [
+          encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+          options.expires ? '; expires=' + options.expires.toUTCString() : emptyString, // use expires attribute, max-age is not supported by IE
+          options.path    ? '; path=' + options.path : emptyString,
+          options.domain  ? '; domain=' + options.domain : emptyString,
+          options.secure  ? '; secure' : emptyString
+        ].join(emptyString)
+      );
     }
 
     // key and possibly options given, get cookie...
@@ -40,8 +42,12 @@
 
     var pairs = document.cookie.split('; ');
     for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
-      if (decode(pair[0]) === key) return decode(pair[1] || ''); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
+      if (decode(pair[0]) === key) return decode(pair[1] || emptyString); // IE saves cookies with empty string as "c; ", e.g. without "=" as opposed to EOMB, thus pair[1] may be undefined
     }
-    return null;
+    return nil;
   };
-})(jQuery);
+})(
+  jQuery,
+  null,
+  ''
+);
